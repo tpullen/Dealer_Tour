aa77385254f906039aaf5deab32e902b62236dbc
Unknown <t.r-pullen@hotmail.co.uk>
2018-01-01 17:29:26 +0000

    Initial Commit
    Basic output from marzipano

---
+++ data.js
@@ -0,0 +1,276 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-exterior",
+      "name": "EXTERIOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5898,
+      "initialViewParameters": {
+        "yaw": -2.160064307393526,
+        "pitch": -0.16920370311862243,
+        "fov": 1.3199252466489146
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5480042002488457,
+          "pitch": 0.27558820187240407,
+          "rotation": 0,
+          "target": "1-interior-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-interior-1",
+      "name": "INTERIOR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.929655381901517,
+        "pitch": 0.2104648971268368,
+        "fov": 0.9585953299704323
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4185401495026895,
+          "pitch": 0.44649145503603194,
+          "rotation": 0,
+          "target": "2-interior-2"
+        },
+        {
+          "yaw": -1.5435279228615606,
+          "pitch": 0.419896287257675,
+          "rotation": 0,
+          "target": "0-exterior"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1377265242498957,
+          "pitch": -0.06293323248046612,
+          "title": "AUDI A6 ALLROAD",
+          "title": "AUDI A5",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a6_allroad/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": -1.1572852443325594,
+          "pitch": 0.01987540885224348,
+          "title": "AUDI A5",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a5/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": -0.638220154401095,
+          "pitch": -0.023608628740664273,
+          "title": "AUDI Q2",
+          "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/q2/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": -0.37265720435967253,
+          "pitch": -0.03205600764280092,
+          "title": "AUDI A3 SPORTBACK",
+          "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a3/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 0.09684829991007149,
+          "pitch": -0.06979888882118601,
+          "title": "AUDI A4 AVANT",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a4/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 0.7132675956984151,
+          "pitch": -0.01176595524655255,
+          "title": "AUDI A7 SPORTBACK",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a7/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 0.43804473973339064,
+          "pitch": -0.02478666022635423,
+          "title": "AUDI A1",
+          "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a1/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 1.2289967866096116,
+          "pitch": -0.06019008479889898,
+          "title": "AUDI Q5",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/q5/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 1.4803183299677087,
+          "pitch": -0.015262852476240596,
+          "title": "AUDI Q3",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/q3/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 1.860286801075718,
+          "pitch": -0.024229639961500737,
+          "title": "AUDI Q7",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/q7/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 2.1868756433506586,
+          "pitch": 0.08458669729042612,
+          "title": "AUDI A4",
+          "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a4_2/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-interior-2",
+      "name": "INTERIOR 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.0263003425106296,
+        "pitch": 0.13483011391153887,
+        "fov": 1.125683494742434
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.833170083512453,
+          "pitch": 0.5447556267959754,
+          "rotation": 0.7853981633974483,
+          "target": "1-interior-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3123456399421016,
+          "pitch": -0.09946047521956913,
+          "title": "AUDI Q7",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/q7/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": -2.914897302877792,
+          "pitch": -0.06601801756084669,
+          "title": "AUDI Q3",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/q3/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 2.5074388819917104,
+          "pitch": -0.20235657098222326,
+          "title": "AUDI Q5",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/q5/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 1.2493597995431802,
+          "pitch": -0.02712967649931919,
+          "title": "AUDI A6",
+        "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a6/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 0.6764775472802889,
+          "pitch": -0.016765520955065227,
+          "title": "AUDI A1",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a1/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": -0.1952715413681183,
+          "pitch": 0.014918778773505892,
+          "title": "AUDI A7 SPORTBACK",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a7/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": 0.28752954583292123,
+          "pitch": -0.014966762280010926,
+          "title": "AUDI A4 AVANT",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a4/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": -0.5357339659924207,
+          "pitch": 0.002073026801296507,
+          "title": "AUDI A5",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a5/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": -0.8421273477212399,
+          "pitch": 0.003541273711997306,
+          "title": "AUDI A6 ALLROAD",
+          "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a6_allroad/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        },
+        {
+          "yaw": -1.3466993580885287,
+          "pitch": -0.014320730338088339,
+          "title": "AUDI A4",
+           "text": "<div style='padding: 70% 0 0 0; width: 100%; position: relative;''><iframe src='http://motorgomez360.com/audi/a4_2/index.html' frameborder='0' scrolling='no' style='background-color: #fff; border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;' allowfullscreen></iframe></div>"
+        }
+      ]
+    }
+  ],
+  "name": "AUDI MOTORGOMEZ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
